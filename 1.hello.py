aacb8fc9042b74f87eac9136882c6758f8f00612
fadiduad78 <120588609+fadiduad78@users.noreply.github.com>
2026-07-03 23:47:16 +0500

    Add files via upload

---
+++ 1.hello.py
@@ -0,0 +1,52 @@
+# print('hello')
+
+# x = 54 
+# y = "Fahad"
+
+
+# # IF ELSE Grading System
+# marks = 76
+# if marks >= 90 :
+#     print("You got A+!")
+# elif marks >= 80 :
+#     print("You got B+!")
+# elif marks >= 70 :
+#     print("You got C+")
+# elif marks >= 60 :
+#     print("You got D+!")
+# elif marks >= 50 :
+#     print("You got E+")
+# else :
+#     print("you are fail")
+
+
+# # Loops table of 5
+
+# for i in range(1,11):
+#     this = i*5
+#     print("5 *" , i ,"=" , this )
+
+
+# # while loop 
+
+# password = ""
+
+# while password != "1234":
+#     password = input("Enter password: ")
+
+
+# # while loop table 
+
+# i = 1
+# while i <=10:
+#     print(f"5 *{i} = {5*i}")
+#     i = i+1
+
+
+
+
+    
+
+m = "14123"
+
+len(m)
